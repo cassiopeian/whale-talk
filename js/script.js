de709d3c271b25482847d8ff46308f0bdfad7e17
cassiopeia <cassiopeia.neely@gmail.com>
2020-07-21 22:58:31 -0400

    submit.onclick established
    » Set the code inside an .onclick, and created an input variable that grabs text from the #human-text textarea.
    » Translations are all logged, so users can see them listed in the console.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-const input = 'Can you tell me what is going on here?';
-const vowels = ['a', 'e', 'i', 'o', 'u'];
-let resultArray = [];
+const submit = document.getElementById('submit');
+const reset = document.getElementById('reset');
 
-for (let i = 0; i < input.length; i++) {
-  for (let x = 0; x < vowels.length; x++) {
-    if (input[i] === vowels[x]) {
-      if (input[i] === 'e') {
-        resultArray.push('ee');
-      }
-          else if (input[i] === 'u') {
-            resultArray.push('uu');
-          }
-          else {
-            resultArray.push(input[i]);
-          }
+// When you click the Translate button, the code below will parse through the text input in the #human-text textarea. Vowels are pulled put and pushed into the result array, and E's and U's are doubled.
+submit.onclick = () => {
+    let input = document.getElementById('human-text').value;
+    let whaleText = document.getElementById('whale-text');
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let resultArray = [];
+
+    for (let i = 0; i < input.length; i++) {
+        for (let x = 0; x < vowels.length; x++) {
+            if (input[i] === vowels[x]) {
+                if (input[i] === 'e') {
+                    resultArray.push('ee');
+                } else if (input[i] === 'u') {
+                    resultArray.push('uu');
+                } else {
+                    resultArray.push(input[i]);
+                }
+            }
         }
-      }
     }
-
-console.log(resultArray.join('').toUpperCase());
+    whaleText.innerHTML = resultArray.join('').toUpperCase();
+    console.log(`YOU SAID: ${input}`);
+    console.log(`WHALE TRANSLATION: ${resultArray.join('').toUpperCase()}.`);
+}
